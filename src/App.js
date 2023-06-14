6f037296071e649379e7c898a9e8a8b9b27ac566
Nicholas Cox <xockcin@gmail.com>
2023-06-14 09:31:18 -0400

    added token switch function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ import { useState } from 'react';
 function App() {
   const [currentByte, setCurrentByte] = useState(0)
 
+  const doToken = (number, token, size) => {
+    switch (token) {
+      case "+":
+        return (number + 1) % size;
+      case "<":
+        return (number * 2) % size;
+      case "~":
+        return size - 1 - number;
+      case ">":
+        return Math.floor(number / 2);
+      case "-":
+        if (number === 0) {
+          return 255
+        }
+        else {
+          return number - 1
+        }
+      default:
+        console.log("Invalid Token")
+    }
+  }
+
   const byteFromNumber = (number) => {
     const newByte = [];
     for (let i = 0; i < 8; i++) {
