b74861662a012d49580616f5f102e95f6bc903b8
Nicholas Cox <xockcin@gmail.com>
2023-06-14 09:24:29 -0400

    added byteFromNumber function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import { useState } from 'react';
 
 function App() {
   const [currentByte, setCurrentByte] = useState(0)
+
+  const byteFromNumber = (number) => {
+    const newByte = [];
+    for (let i = 0; i < 8; i++) {
+      newByte[i] = !!(number & (1 << i));
+    }
+    return newByte;
+  }
+
   return (
     <div className="App">
       <h1>hello</h1>
