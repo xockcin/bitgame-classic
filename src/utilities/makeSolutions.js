9b4d1887d852207d0b5e37803dde380d4eedcc7e
Nicholas Cox <xockcin@gmail.com>
2023-06-20 14:10:01 -0400

    makeSolutions now one nice function, exports data

--- src/utilities/makeSolutions.js
+++ src/utilities/makeSolutions.js
@@ -1,83 +1,80 @@
 const TOKENS = ["+", "<", "~", ">", "-"];
-const SIZE = 256;
+const SIZE = 16;
 
 const _ = require("lodash");
 
-const doToken = (number, token) => {
-  switch (token) {
-    case "<":
-      return (number * 2) % SIZE;
-    case ">":
-      return Math.floor(number / 2);
-    case "~":
-      return SIZE - 1 - number;
-    case "+":
-      return number === SIZE - 1 ? 0 : number + 1;
-    case "-":
-      return number === 0 ? SIZE - 1 : number - 1;
-    case "!":
-      return 255;
-    case " ":
-      return 0;
-    default:
-      return null
-  }
-};
+const makeAllSolutions = (size, tokens) => {
+  //initialize solutions grid
+  let solutionsGrid = [...Array(SIZE)].map((e) => Array(SIZE));
 
-//initialize solutions grid
-let solutionsGrid = [...Array(SIZE)].map((e) => Array(SIZE));
-for (let i = 0; i < SIZE; i++) {
-  for (let j = 0; j < SIZE; j++) {
-    solutionsGrid[i][j] = {
-      origin: i,
-      result: j,
-      solution: "none",
-      steps: []
-    };
-  }
-}
-
-// in pairs where the origin and goal are the 
-// same number, set the solution to an empty string
-const zeroSteps = () => {
   for (let i = 0; i < SIZE; i++) {
-    solutionsGrid[i][i].solution = ""
+    for (let j = 0; j < SIZE; j++) {
+      solutionsGrid[i][j] = {
+        origin: i,
+        result: j,
+        solution: "none",
+        steps: []
+      };
+    }
   }
-}
 
-const oneSteps = () => {
-  for (let i = 0; i < SIZE; i++) {
-    for (let token of TOKENS) {
-      const result = doToken(i, token);
-      if (solutionsGrid[i][result].solution === "none") {
-        solutionsGrid[i][result].solution = token;
-        solutionsGrid[i][result].steps.push({token: token, number: result})
+  const doToken = (number, token) => {
+    switch (token) {
+      case "<":
+        return (number * 2) % SIZE;
+      case ">":
+        return Math.floor(number / 2);
+      case "~":
+        return SIZE - 1 - number;
+      case "+":
+        return number === SIZE - 1 ? 0 : number + 1;
+      case "-":
+        return number === 0 ? SIZE - 1 : number - 1;
+      case "!":
+        return 255;
+      case " ":
+        return 0;
+      default:
+        return null
+    }
+  };
+
+  // in pairs where the origin and goal are the 
+  // same number, set the solution to an empty string
+  const zeroSteps = () => {
+    for (let i = 0; i < SIZE; i++) {
+      solutionsGrid[i][i].solution = ""
+    }
+  }
+
+  const oneSteps = () => {
+    for (let i = 0; i < SIZE; i++) {
+      for (let token of TOKENS) {
+        const result = doToken(i, token);
+        if (solutionsGrid[i][result].solution === "none") {
+          solutionsGrid[i][result].solution = token;
+          solutionsGrid[i][result].steps.push({token: token, number: result})
+        }
       }
     }
   }
-};
-
-// does a new "step" every time you run it.
-// so the first time it does all the two-step
-// solutions, the second time it does all the
-// three-step solutions, and so on.
-const addSteps = () => {
-  // flat array containing all solved pairs
-  const solvedPairs = _.flatten(solutionsGrid).filter((item) => item.solution !== "none")
-  for (let pair of solvedPairs) {
-    // build each new solution atop an existing one
-    const {origin, result, solution} = pair
-    for (let token of TOKENS) {
-      const newResult = doToken(result, token)
-      if (solutionsGrid[origin][newResult].solution === "none") {
-        solutionsGrid[origin][newResult].solution = solution + token
-        solutionsGrid[origin][newResult].steps.push({ token: token, number: newResult });
+  
+  const addSteps = () => {
+    // flat array containing all solved pairs
+    const solvedPairs = _.flatten(solutionsGrid).filter((item) => item.solution !== "none")
+    for (let pair of solvedPairs) {
+      // build each new solution atop an existing one
+      const {origin, result, solution} = pair
+      for (let token of TOKENS) {
+        const newResult = doToken(result, token)
+        if (solutionsGrid[origin][newResult].solution === "none") {
+          solutionsGrid[origin][newResult].solution = solution + token
+          solutionsGrid[origin][newResult].steps.push({ token: token, number: newResult });
+        }
       }
     }
   }
-}
 
-const getAllSolutions = () => {
   zeroSteps();
   oneSteps();
   let steps = 2;
@@ -89,19 +86,8 @@ const getAllSolutions = () => {
     addSteps();
     steps++;
   }
-  return steps;
-};
 
-getAllSolutions();
-
-const sortSolutionsByLength = (grid) => {
-    const allSolutionsArray = _.flatten(grid)
-    let sortedSolutions = new Array(10).fill([])
-    allSolutionsArray.map(item => sortedSolutions[item.solution.length - 1].push(item))
-    return sortedSolutions
+  return solutionsGrid
 }
 
-console.log(solutionsGrid[34][79])
-console.log(solutionsGrid[211][0])
-console.log(solutionsGrid[1][32])
-console.log(solutionsGrid[34][35])
+export const allSolutions = makeAllSolutions(SIZE, TOKENS)
